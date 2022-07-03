8908b5df7345863a67b697894634c4e8232a60d6
pravalika2222 <107530566+pravalika2222@users.noreply.github.com>
2022-07-03 20:35:35 +0530

    Add files via upload

---
+++ data_type.py
@@ -0,0 +1,42 @@
+'''int data type'''    
+# a=(3)
+# print(a,type(a))      #  3   ('int type')
+# print(float(a))    # 3.0
+# print(str(a))      # 3
+
+'''float date type '''
+# p=(2.09)
+# print(p,type(p))      #  2.09  ('float type')
+# print(int(p))         #    2
+# print(str(p))         #   2.09  
+
+'''string type'''
+# r='hello prava'
+# print(r,type(r))     #   ('string type')
+# print(r)            #   hello prava
+
+'''list date type'''    # list is mutable modification can be made and it is a square bracket[]
+# s=[1,4]
+# print(s,type(s))       #    [ list type]
+# print(s)               #    [1,4]
+
+'''tuple date type'''    # a varible holding a single value with a comma is know as 'tuple'  & (,)
+# g=(23,898)
+# print(g,type(g))       #   (tuple type)
+# print(g)               #  (23,898)
+
+'''set date type'''      # a set is a curly bracket with an elements is know as 'set'   & {1,2}
+# d={10,3}
+# print(d,type(d))        #    {set type}
+# print(d)                #    {10,3}
+
+'''dictionary date type'''  # dict is the combination of keyvalue 
+# k={1:'so',
+#    2:'sweet'}
+# print(k,type(k))      #  {dict type}
+# print(k[1])           #   so
+# print(k[2])           #    sweet
+
+'''range date type'''    # 
+# for n in range(5):
+#     print(n)     #  output will get in the vertical 
